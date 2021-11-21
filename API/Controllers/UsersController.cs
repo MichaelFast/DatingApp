ecd8f24d268598ecfb04eabb8b2cb4785343ea18
MichaelFast <mica.fast@gmail.com>
2021-11-21 09:32:35 +0200

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,17 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -29,9 +27,17 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
-      var users = await _userRepository.GetMembersAsync();
+      var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
+      userParams.CurrentUsername = user.UserName;
+
+      if (string.IsNullOrEmpty(userParams.Gender))
+        userParams.Gender = user.Gender == "male" ? "female" : "male";
+
+
+      var users = await _userRepository.GetMembersAsync(userParams);
+      Response.AddPginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
       return Ok(users);
     }
 
